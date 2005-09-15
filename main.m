321f068b7cfbe325829ec28e76cc98d550d07dea
sboisson <sboisson@127852f0-686f-4d33-a4f6-f272b2951c41>
2005-09-15 18:15:43 +0000

    Disabled exception catching
    git-svn-id: file:///Users/kuba/wc/chmox.svn/trunk@63 127852f0-686f-4d33-a4f6-f272b2951c41

--- main.m
+++ main.m
@@ -16,19 +16,24 @@
 // along with Foobar; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //
-// $Revision: 1.2 $
+// $Revision: 1.3 $
 //
 
 #import <Cocoa/Cocoa.h>
 
 int main( int argc, const char *argv[] )
 {
+/*
     @try {
+*/
 	return NSApplicationMain( argc, argv );
+/*
     }
     @catch ( NSException *e ) {
 	NSLog( @"Exception: %@", e );
+        @throw e;
     }
     @finally {
     }
+    */
 }
